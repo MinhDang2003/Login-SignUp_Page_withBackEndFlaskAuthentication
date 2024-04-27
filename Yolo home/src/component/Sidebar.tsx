82f315c10b37cec158153048b704f927d1d0950d
Minh Le <minhle.q2303@gmail.com>
2024-04-28 03:33:37 +0700

    add log out

--- Yolo home/src/component/Sidebar.tsx
+++ Yolo home/src/component/Sidebar.tsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
-const Sidebar = () => {
+import useLogout from "../hooks/useLogout";
+function Sidebar() {
+    const logout = useLogout();
     return (
-        <div className="flex font-poppins">
+        <div className="flex">
             <nav className="w-full h-screen bg-[#faf0dc] ">
                 <ul className="flex flex-col w-full">
-
                     <li className="w-full">
-                        <Link to="/Dashboard" className="block p-3 text-black  hover:text-[#0047AB]  ">Dashboard</Link>
+                        <Link to="/Dashboard" className="block p-3 text-black hover:text-[#0047AB] ">
+                            Dashboard
+                        </Link>
                     </li>
                     <li className="w-full">
-                        <Link to="/" className="block p-3 text-black text hover:text-[#0047AB]">Home</Link>
+                        <Link to="/" className="block p-3 text-black text hover:text-[#0047AB]">
+                            Home
+                        </Link>
                     </li>
+                    <button  className="block p-3 text-black hover:text-[#0047AB] bg-transparent hover:border-transparent text-left  ">
+                        Log Out
+                    </button>
                 </ul>
             </nav>
         </div>
     );
-};
+}
+
 export default Sidebar;
