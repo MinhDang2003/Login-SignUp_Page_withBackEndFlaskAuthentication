e86c5a43e441663657a024488f6d46efc3e5c407
Minh Le <minhle.q2303@gmail.com>
2024-05-06 17:32:02 +0700

    Revert "123"
    This reverts commit 7f4f0453032cff596e621c78afd870b58cfdd95e.

--- Yolo home/src/component/Sidebar.tsx
+++ Yolo home/src/component/Sidebar.tsx
@@ -1,7 +1,6 @@
 import { useNavigate, Link } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
 import HCMUTlogo from "../assets/OIP.png";
-import { render } from "react-dom";
 
 function Sidebar() {
 	const logout = useLogout();
@@ -11,23 +10,24 @@ function Sidebar() {
 		navigate("/login");
 	};
 	return (
-		<div className="h-10">
-			<nav className="bg-[#FEFAF6] h-10">
-				<ul className=" flex items-center">
-                    <li >
-                        <img className="h-8 items-center" src={HCMUTlogo} alt="HCMUT logo" />
+		
+		<div className="flex ">
+			<nav className="w-full h-screen bg-[#EADBC8] rounded-r-3xl shadow-md">
+				<ul className="flex flex-col w-full">
+                    <li className="w-full">
+                        <img src={HCMUTlogo} alt="HCMUT logo" className="w-1/6  mx-auto" />
                     </li>
-					<li  >
+					<li className="w-full">
 						<Link to="/Dashboard" className="block p-3  ">
 							Dashboard
 						</Link>
 					</li>
-					<li >
+					<li className="w-full">
 						<Link to="/" className="block p-3 ">
 							Home
 						</Link>
 					</li>
-					<div >
+					<div className="w-full">
 						<Link to="#" className="block p-3 hover:bg-transparent hover:text-[#26577C]  navItem" onClick={signOut}>
 							Log Out
 						</Link>
